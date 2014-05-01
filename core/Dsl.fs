1ac5515a1f0924d9d84c4dd8eb24b264692b9bf4
Peter Strøiman <peter@stroiman.com>
2014-05-01 10:47:19 +0100

    Various renamings to make core more readable

--- core/Dsl.fs
+++ core/Dsl.fs
@@ -52,19 +52,19 @@ module ExampleGroup =
     let addTearDown tearDown ctx = { ctx with TearDowns = tearDown::ctx.TearDowns }
     let addChildContext child ctx = { ctx with ChildGroups = child::ctx.ChildGroups }
 
-    let rec perform_setup contexts =
-        match contexts with
+    let rec performSetup exampleGroups =
+        match exampleGroups with
             | [] -> ()
             | head::tail ->
-                perform_setup tail
+                performSetup tail
                 head.Setups |> List.iter (fun y -> y())
     
-    let rec perform_teardown contexts =
-        match contexts with
+    let rec performTearDown exampleGroups =
+        match exampleGroups with
             | [] -> ()
             | head::tail ->
                 head.TearDowns |> List.iter (fun y -> y())
-                perform_teardown tail
+                performTearDown tail
     
     let rec run exampleGroups (results : TestReport) =
         let exampleGroup = exampleGroups |> List.head
@@ -74,16 +74,16 @@ module ExampleGroup =
             | head::[] -> head
             | head::tail ->sprintf "%s %s" (printNameStack(tail)) head
 
-        exampleGroup.Examples |> List.rev |> List.iter (fun x -> 
-            let nameStack = x.Name :: (exampleGroups |> List.map (fun x -> x.Name) |> List.filter (fun x -> x <> null))
+        exampleGroup.Examples |> List.rev |> List.iter (fun example -> 
+            let nameStack = example.Name :: (exampleGroups |> List.map (fun x -> x.Name) |> List.filter (fun x -> x <> null))
             let name = printNameStack(nameStack)
             try
                 try
-                    let context = x.MetaData |> TestContext.create
-                    perform_setup exampleGroups
-                    x.Test context
+                    let context = example.MetaData |> TestContext.create
+                    performSetup exampleGroups
+                    example.Test context
                 finally
-                    perform_teardown exampleGroups
+                    performTearDown exampleGroups
                 results.reportTestName name Success
             with
             | PendingError -> results.reportTestName name Pending
