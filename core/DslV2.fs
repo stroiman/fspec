7016506db41365621143442061063ad1d3e4d559
Peter Strøiman <peter@stroiman.com>
2014-05-01 16:03:00 +0200

    Array based 'describe' and 'it'

--- core/DslV2.fs
+++ core/DslV2.fs
@@ -1,1 +1,16 @@
 ﻿module FSpec.Core.DslV2
+open FSpec.Core.DomainTypes
+
+type Operation =
+    | AddExampleOperation of Example.T
+
+let it name func = AddExampleOperation <| Example.create name func
+    
+let applyOperation grp op =
+    match op with
+    | AddExampleOperation example -> grp |> ExampleGroup.addExample example
+
+let describe name operations =
+    let grp = ExampleGroup.create name
+    operations |> List.fold applyOperation grp
+    
--- selftests/DslV2Specs.fs
+++ selftests/DslV2Specs.fs
@@ -1,7 +1,17 @@
 ﻿module FSpec.SelfTests.DslV2Specs
 open FSpec.Core.DomainTypes
+open FSpec.Core.DslV2
+open FSpec.Core.MatchersV2
 
 let specs =
     ExampleGroup.create "Group"
-    |> ExampleGroup.addExample (Example.create "Example" (fun _ ->
-        ()))
+    |> ExampleGroup.addExample (Example.create "Example" <| fun _ ->
+        let group = 
+            describe "Group" [
+                it "Test" <| fun _ -> ()
+            ]
+        group.Name |> should equal "Group"
+        group.Examples.Length |> should equal 1
+        let [example] = group.Examples
+        example.Name |> should equal "Test"
+        )
