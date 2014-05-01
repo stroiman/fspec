95f4adcff2821ad069ba107e461a5667dda9c643
Peter Strøiman <peter@stroiman.com>
2014-05-01 23:06:29 +0200

    Dsl for nested context

--- core/DslV2.fs
+++ core/DslV2.fs
@@ -3,14 +3,20 @@ open FSpec.Core.DomainTypes
 
 type Operation =
     | AddExampleOperation of Example.T
+    | AddExampleGroupOperation of ExampleGroup.T
 
 let it name func = AddExampleOperation <| Example.create name func
     
 let applyOperation grp op =
     match op with
     | AddExampleOperation example -> grp |> ExampleGroup.addExample example
+    | AddExampleGroupOperation childGrp -> grp |> ExampleGroup.addChildContext childGrp
 
 let describe name operations =
     let grp = ExampleGroup.create name
     operations |> List.fold applyOperation grp
+    
+let context name operations =
+    let grp = describe name operations
+    grp |> AddExampleGroupOperation
     
--- selftests/DslV2Specs.fs
+++ selftests/DslV2Specs.fs
@@ -3,15 +3,30 @@ open FSpec.Core.DomainTypes
 open FSpec.Core.DslV2
 open FSpec.Core.MatchersV2
 
+let pass = fun _ -> ()
+
 let specs =
     describe "Example building DSL" [
         it "builds example groups" <| fun _ ->
             let group = 
                 describe "Group" [
-                    it "Test" <| fun _ -> ()
+                    it "Test" pass
                 ]
             group.Name |> should equal "Group"
             group.Examples.Length |> should equal 1
             let [example] = group.Examples
             example.Name |> should equal "Test"
+
+        it "builds dsl with nested example group" <| fun _ ->
+            let group =
+                describe "Group" [
+                    context "Context" [
+                        it "Test" pass
+                ]]
+            match group.ChildGroups with
+            | [grp] -> 
+                match grp.Examples with
+                | [ex] -> ex.Name |> should equal "Test"
+                | _ -> failwith "Bad examples"
+            | _ -> failwith "Bad groups"
     ]
