ac629cb025dc847580f182ff63227bf6eb3ec551
Peter Strøiman <peter@stroiman.com>
2014-05-01 22:55:52 +0200

    DSL v2 spec to use DSL v2

--- selftests/DslV2Specs.fs
+++ selftests/DslV2Specs.fs
@@ -4,14 +4,14 @@ open FSpec.Core.DslV2
 open FSpec.Core.MatchersV2
 
 let specs =
-    ExampleGroup.create "Group"
-    |> ExampleGroup.addExample (Example.create "Example" <| fun _ ->
-        let group = 
-            describe "Group" [
-                it "Test" <| fun _ -> ()
-            ]
-        group.Name |> should equal "Group"
-        group.Examples.Length |> should equal 1
-        let [example] = group.Examples
-        example.Name |> should equal "Test"
-        )
+    describe "Example building DSL" [
+        it "builds example groups" <| fun _ ->
+            let group = 
+                describe "Group" [
+                    it "Test" <| fun _ -> ()
+                ]
+            group.Name |> should equal "Group"
+            group.Examples.Length |> should equal 1
+            let [example] = group.Examples
+            example.Name |> should equal "Test"
+    ]
