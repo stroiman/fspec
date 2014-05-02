aa3181348a3b74caea713e9081b099e36b87d637
Peter Strøiman <peter@stroiman.com>
2014-05-02 08:29:41 +0200

    Moved dsl build metadata test
    This was a test of the old dsl builder, so it has been grouped with those

--- selftests/MetaDataSpecs.fs
+++ selftests/MetaDataSpecs.fs
@@ -5,10 +5,4 @@ open Matchers
 open DslHelper
 
 let specs =
-    describe "TestBuilder" <| fun _ ->
-        describe "TestMetaData" <| fun _ ->
-            it "is initialized from test" <| fun _ ->
-                let sut = TestCollection()
-                sut.it_ [("answer", 42)] "dummy" <| fun _ -> ()
-                sut.examples.Examples.Head.MetaData.get "answer" |> should equal 42
-                
+    ()               
--- selftests/Selftests.fs
+++ selftests/Selftests.fs
@@ -8,6 +8,12 @@ let specs =
     describe "Reporting" <| fun _ ->
         let sut = DslHelper()
 
+        describe "TestMetaData" <| fun _ ->
+            it "is initialized from test" <| fun _ ->
+                let sut = TestCollection()
+                sut.it_ [("answer", 42)] "dummy" <| fun _ -> ()
+                sut.examples.Examples.Head.MetaData.get "answer" |> should equal 42
+
         describe "summary" <| fun _ ->
             it "reports test success" <| fun _ ->
                 sut.it "Is a success" pass
