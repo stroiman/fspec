31cc2af6d48634b6f9e4de1c7de72858d24c4f70
Peter Strøiman <peter@stroiman.com>
2014-05-18 12:23:44 +0200

    Deleted dead code

--- core/TestReport.fs
+++ core/TestReport.fs
@@ -72,7 +72,6 @@ module TreeReporter =
         |> List.rev
         |> (print [] [])
 
-
     let beginGroup printer exampleGroup report =
         { report with Groups = exampleGroup :: report.Groups }
 
@@ -127,46 +126,3 @@ module TreeReporter =
         BeginTestRun = fun () -> Zero 
     }
     let createReporter = createReporterWithPrinter consolePrinter
-    
-module Report =
-    type T = {
-        output: string list;
-        failed: string list;
-        pending: string list;
-    }
-
-    let create () = {
-        output = [];
-        failed = [];
-        pending = [];
-    }
-
-    let addOutput report output = { report with output = output::report.output }
-    let addFail report fail = { report with failed = fail::report.failed }
-    let addPending report pending = { report with pending = pending::report.pending }
-
-    let failed report = report.failed
-    let output report = report.output
-    let success report = report.failed = []
-    let reportTestName name result report =
-        let name' = match result with
-                    | Success -> sprintf "%s - passed" name
-                    | Pending -> sprintf "%s - pending" name
-                    | Error(ex) -> sprintf "%s - failed - %s" name (ex.ToString())
-                    | Failure(errorInfo) -> 
-                        sprintf "%s - failed - %s" name errorInfo.Message
-        let report' = match result with
-                        | Success -> report
-                        | Pending -> addPending report name'
-                        | _ -> addFail report name'
-        addOutput report' name'
-
-    let summary report =
-        let noOfFails = report.failed |> List.length
-        let noOfRuns = report.output |> List.length
-        let noOfPendings = report.pending |> List.length
-        if (noOfPendings > 0) then
-            sprintf "%d run, %d failed, %d pending" noOfRuns noOfFails noOfPendings
-        else
-            sprintf "%d run, %d failed" noOfRuns noOfFails
-
