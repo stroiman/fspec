b7d14a3471ba5b811c4e8974da7f3ef5ca646bfb
Peter Strøiman <peter@stroiman.com>
2014-06-01 12:57:20 -0700

    Restructure runner specs
    All specs that have to do with the order in which examples are executed have
    been placed in a common group

--- selftests/RunnerSpecs.fs
+++ selftests/RunnerSpecs.fs
@@ -17,106 +17,105 @@ let shouldRecord expected grp =
 
 let specs =
     describe "Test runner" [
-        before <| clearCallList
+        describe "execution order" [
+            before <| clearCallList
 
-        context "test execution order" [
-            it "tests execute in the order they appear" <| fun _ ->
-                anExampleGroup
-                |> withExampleCode (record "test 1")
-                |> withExampleCode (record "test 2")
-                |> shouldRecord ["test 1"; "test 2"]
+            describe "of examples" [
+                it "executes examples in the order they appear" <| fun _ ->
+                    anExampleGroup
+                    |> withExampleCode (record "test 1")
+                    |> withExampleCode (record "test 2")
+                    |> shouldRecord ["test 1"; "test 2"]
 
-            it "child contexts execute in the order they appear" (fun _ ->
-                anExampleGroup
-                |> withNestedGroup (
-                    withExampleCode (record "test 1"))
-                |> withNestedGroup (
-                    withExampleCode (record "test 2"))
-                |> shouldRecord ["test 1"; "test 2"]
-            )
-        ]
+                it "executes child groups in the order they appear" (fun _ ->
+                    anExampleGroup
+                    |> withNestedGroup (
+                        withExampleCode (record "test 1"))
+                    |> withNestedGroup (
+                        withExampleCode (record "test 2"))
+                    |> shouldRecord ["test 1"; "test 2"]
+                )
+            ]
 
-        context "General setup/test/teardown handling" [
-            it "runs the sequence before, spec, teardown" <| fun _ ->
-                anExampleGroup
-                |> withSetupCode (record "setup")
-                |> withTearDownCode (record "tearDown")
-                |> withExampleCode (record "test")
-                |> shouldRecord ["setup"; "test"; "tearDown"]
+            describe "of setup and teardown" [
+                it "runs setup/teardown once for each test" <| fun _ ->
+                    anExampleGroup
+                    |> withSetupCode (record "setup")
+                    |> withTearDownCode (record "tear down")
+                    |> withExampleCode (record "test 1")
+                    |> withExampleCode (record "test 2")
+                    |> shouldRecord
+                        [ "setup"; "test 1"; "tear down";
+                          "setup"; "test 2"; "tear down"]
+                    
+                describe "setup" [
+                    it "is executed before the example" <| fun _ ->
+                        anExampleGroup
+                        |> withSetupCode (record "setup")
+                        |> withExampleCode (record "test")
+                        |> shouldRecord ["setup"; "test"]
 
-            it "runs outer setup before inner setup" <| fun _ ->
-                anExampleGroup
-                |> withSetupCode (record "outer setup")
-                |> withNestedGroup (
-                    withSetupCode (record "inner setup")
-                    >> withExampleCode (record "test"))
-                |> shouldRecord [ "outer setup"; "inner setup"; "test" ]
+                    it "is executed for example in child group" <| fun _ ->
+                        anExampleGroup
+                        |> withSetupCode (record "outer setup")
+                        |> withNestedGroup (
+                            withSetupCode (record "inner setup")
+                            >> withExampleCode (record "test"))
+                        |> shouldRecord [ "outer setup"; "inner setup"; "test" ]
 
-            it "runs inner tear down before outer tear down" <| fun _ ->
-                anExampleGroup
-                |> withTearDownCode (record "outer tear down")
-                |> withNestedGroup (
-                    withTearDownCode (record "inner tear down")
-                    >> withExampleCode (record "test"))
-                |> shouldRecord ["test"; "inner tear down"; "outer tear down"]
+                    it "is not executed for subling group examples" <| fun _ ->
+                        anExampleGroup
+                        |> withNestedGroup (
+                            withSetupCode (record "setup"))
+                        |> withNestedGroup (
+                            withExampleCode (record "sibling test"))
+                        |> shouldRecord ["sibling test"]
 
-            it "runs setup/teardown once for each test" <| fun _ ->
-                anExampleGroup
-                |> withSetupCode (record "setup")
-                |> withTearDownCode (record "tear down")
-                |> withExampleCode (record "test 1")
-                |> withExampleCode (record "test 2")
-                |> shouldRecord
-                    [ "setup"; "test 1"; "tear down";
-                      "setup"; "test 2"; "tear down"]
-        ]
-            
-        context "setup" [
-            it "is only run in same context, or nested context" <| fun _ ->
-                anExampleGroup
-                |> withSetupCode (record "outer setup")
-                |> withExampleCode (record "outer test")
-                |> withNestedGroup (
-                    withSetupCode (record "inner setup")
-                    >> withExampleCode (record "inner test 1")
-                    >> withExampleCode (record "inner test 2"))
-                |> shouldRecord
-                    ["outer setup"; "outer test";
-                     "outer setup"; "inner setup"; "inner test 1";
-                     "outer setup"; "inner setup"; "inner test 2"]
+                    it "is executed in the order they appear" <| fun _ ->
+                        anExampleGroup
+                        |> withSetupCode (record "setup 1")
+                        |> withSetupCode (record "setup 2")
+                        |> withAnExample
+                        |> shouldRecord ["setup 1";"setup 2"]
+                ]
 
-            it "is executed in the order they appear" <| fun _ ->
-                anExampleGroup
-                |> withSetupCode (record "setup 1")
-                |> withSetupCode (record "setup 2")
-                |> withAnExample
-                |> shouldRecord ["setup 1";"setup 2"]
-        ]
+                describe "tear down" [
+                    it "is executed after the example" <| fun _ ->
+                        anExampleGroup
+                        |> withTearDownCode (record "tearDown")
+                        |> withExampleCode (record "test")
+                        |> shouldRecord ["test"; "tearDown"]
 
-        context "tear down" [
-            it "runs if test fail" <| fun _ ->
-                anExampleGroup
-                |> withTearDownCode (record "tearDown")
-                |> withExamples [ aFailingExample ]
-                |> shouldRecord ["tearDown"]
+                    it "is executed if example fails" <| fun _ ->
+                        anExampleGroup
+                        |> withTearDownCode (record "tearDown")
+                        |> withExamples [ aFailingExample ]
+                        |> shouldRecord ["tearDown"]
 
-            it "runs teardown in the right context" <| fun _ ->
-                anExampleGroup
-                |> withTearDownCode (record "outer tearDown")
-                |> withExampleCode (record "outer test")
-                |> withNestedGroup (
-                    withTearDownCode (record "inner tearDown")
-                    >> withExampleCode (record "inner test"))
-                |> shouldRecord
-                    ["outer test"; "outer tearDown";
-                     "inner test"; "inner tearDown"; "outer tearDown"]
+                    it "is executed for example in child group" <| fun _ ->
+                        anExampleGroup
+                        |> withTearDownCode (record "outer tear down")
+                        |> withNestedGroup (
+                            withTearDownCode (record "inner tear down")
+                            >> withExampleCode (record "test"))
+                        |> shouldRecord ["test"; "inner tear down"; "outer tear down"]
 
-            it "runs in the order it appears" <| fun _ ->
-                anExampleGroup
-                |> withTearDownCode (record "teardown 1")
-                |> withTearDownCode (record "teardown 2")
-                |> withAnExample
-                |> shouldRecord ["teardown 1";"teardown 2"]
+                    it "is not executed for sibling group examples" <| fun _ ->
+                        anExampleGroup
+                        |> withNestedGroup (
+                            withTearDownCode (record "tearDown"))
+                        |> withNestedGroup (
+                            withExampleCode (record "sibling test"))
+                        |> shouldRecord ["sibling test"]
+                    
+                    it "runs in the order it appears" <| fun _ ->
+                        anExampleGroup
+                        |> withTearDownCode (record "teardown 1")
+                        |> withTearDownCode (record "teardown 2")
+                        |> withAnExample
+                        |> shouldRecord ["teardown 1";"teardown 2"]
+                ]
+            ]
         ]
 
         describe "context cleanup" [
