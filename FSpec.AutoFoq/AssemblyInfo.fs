00792ba6612d07dcd8214e9a21d708db78f656b9
Peter Strøiman <peter@stroiman.com>
2014-06-25 13:23:01 +0200

    v-0.1.1

--- FSpec.AutoFoq/AssemblyInfo.fs
+++ FSpec.AutoFoq/AssemblyInfo.fs
@@ -2,9 +2,9 @@
 open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("FSpec.AutoFoq")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.1.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.0"
+    let [<Literal>] Version = "0.1.1"
