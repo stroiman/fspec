a811dfd5679b4425d6db22b9a35e4111eed4eaa4
Peter Strøiman <peter@stroiman.com>
2014-12-03 10:27:11 +0100

    v-0.3.2

--- FSpec.MbUnitWrapper/AssemblyInfo.fs
+++ FSpec.MbUnitWrapper/AssemblyInfo.fs
@@ -3,7 +3,7 @@ open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 [<assembly: AssemblyTitle("FSpec")>]
-[<assembly: AssemblyCopyright("(c) 2014 by Peter Strøiman")>]
-[<assembly: AssemblyVersion("0.3.1.0")>]
-[<assembly: AssemblyFileVersion("0.3.1.0")>]
+[<assembly: AssemblyCopyright("(c) 2014 by Peter Str\u00F8iman")>]
+[<assembly: AssemblyVersion("0.3.2.0")>]
+[<assembly: AssemblyFileVersion("0.3.2.0")>]
 ()
