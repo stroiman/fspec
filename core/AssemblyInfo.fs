0608d17f07843a8a37407b4893aa329a20a7cb69
Peter Strøiman <peter@stroiman.com>
2014-12-03 10:35:03 +0100

    v-0.3.3

--- core/AssemblyInfo.fs
+++ core/AssemblyInfo.fs
@@ -4,6 +4,6 @@ open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 [<assembly: AssemblyTitle("FSpec")>]
 [<assembly: AssemblyCopyright("(c) 2014 by Peter Str\u00F8iman")>]
-[<assembly: AssemblyVersion("0.3.2.0")>]
-[<assembly: AssemblyFileVersion("0.3.2.0")>]
+[<assembly: AssemblyVersion("0.3.3.0")>]
+[<assembly: AssemblyFileVersion("0.3.3.0")>]
 ()
