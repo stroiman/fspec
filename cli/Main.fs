b95b6a3bc2b60f06e9b66d3f3bb1b192198ef123
Peter Strøiman <peter@stroiman.com>
2014-12-15 15:43:56 +0100

    Introduced CommandLineParser
    This should allow us to specify arguments for the runner

--- cli/Main.fs
+++ cli/Main.fs
@@ -2,12 +2,31 @@ module Main
 open System.Reflection
 open FSpec
 open FSpec.TestDiscovery
+open CommandLine
+open CommandLine.Text
+
+type L<'T> = System.Collections.Generic.List<'T>
+
+type Options () =
+    [<ValueList(typeof<L<string>>)>]
+    member val AssemblyFiles : L<string> = (null :> L<string>) with get, set
+
+type ParsedArguments = {
+    AssemblyFiles : string list }
+
+let parseArguments args =
+    let options = Options()
+    let parser = new CommandLine.Parser()
+    match parser.ParseArguments(args, options) with
+    | false -> failwith "Error parsing command line"
+    | true -> { AssemblyFiles = List.ofSeq options.AssemblyFiles }
 
 [<EntryPoint>]
 let main args =
+    let parsedArgs = parseArguments args
     let options = { TreeReporterOptions.Default with PrintSuccess = false }
     let reporter = TreeReporter.create options
-    args
+    parsedArgs.AssemblyFiles
     |> Seq.map (fun assemblyName -> Assembly.LoadFrom(assemblyName))
     |> Seq.mapMany getSpecsFromAssembly
     |> runSpecsWithReporter reporter
---
+++ selftests/CommandLineParserSpecs.fs
@@ -0,0 +1,17 @@
+﻿module FSpec.SelfTest.sCommandLineParserSpecs
+open FSpec
+open FSpec.Dsl
+open FSpec.Matchers
+open Main
+
+let specs =
+    describe "CommandLineParser" [
+        context "Called with two assembly files" [
+            it "loads the two assembly files" (fun _ ->
+                let input = [| "assembly1.dll"; "assembly2.dll" |]
+                let parsedData = parseArguments input
+                let expected = List.ofArray input
+                parsedData.AssemblyFiles |> should (equal expected)
+            )
+        ]
+    ]
