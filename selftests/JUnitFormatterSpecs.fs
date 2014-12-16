26a3975d846fddfb5059f721b563087f8170c6fa
Peter Strøiman <peter@stroiman.com>
2014-12-16 15:55:23 +0100

    Skeleton JUnit output reporter
    The formatter is not in a usable state, but we now have test code that can
    verify that the generated xml is valid according to the schema

---
+++ selftests/JUnitFormatterSpecs.fs
@@ -0,0 +1,14 @@
+﻿module JUnitFormatterSpecs
+open XmlHelpers
+open FSpec
+open FSpec.Dsl
+open FSpec.Matchers
+open FSpec.Formatters
+
+let specs =
+  describe "JUnitFormatter" [
+    it "creates valid xml" (fun _ ->
+      let result = JUnitFormatter.run ()
+      isValidJUnitXml result |> should (equal true)
+    )
+  ]
---
+++ selftests/XmlHelpers.fs
@@ -0,0 +1,27 @@
+﻿module XmlHelpers
+open System.Xml
+open System.Xml.Schema
+open System.Reflection
+
+let assembly = Assembly.GetExecutingAssembly()
+let resourceName = "JUnit.xsd"
+let openSchemaStream () = assembly.GetManifestResourceStream(resourceName)
+
+let isValidJUnitXml xml =
+  let valid = ref true
+  let eventHandler (sender:obj) (e:ValidationEventArgs) =
+    let invalidate () =
+      valid := false
+      printfn "XML error: %s" e.Message
+    match e.Severity with
+    | XmlSeverityType.Error -> invalidate()
+    | XmlSeverityType.Warning -> invalidate()
+    | _ -> ()
+  let document = new XmlDocument()
+  let schemaStream = openSchemaStream()
+  let reader = XmlReader.Create(schemaStream)
+  document.LoadXml xml
+  document.Schemas.Add("", reader) |> ignore
+  document.Validate(new ValidationEventHandler(eventHandler))
+  !valid
+
