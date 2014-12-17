b814670c2afb86e57cb415fa0fd30378e442e9bd
Peter Strøiman <peter@stroiman.com>
2014-12-17 16:57:56 +0100

    Verify precense of test suite element

--- cli/JUnitFormatter.fs
+++ cli/JUnitFormatter.fs
@@ -1,6 +1,10 @@
 ﻿module FSpec.Formatters
 open System.Xml.Linq
 
+type Report = 
+  | ExampleReport of ExampleDescriptor * TestResultType
+  | ExampleGroupReport of ExampleDescriptor * Report list
+
 let xname = XName.Get
 
 module JUnitFormatter =
@@ -8,4 +12,12 @@ module JUnitFormatter =
     let suite = XElement(xname "testsuite", "")
     let elms = XElement(xname "testsuites", XElement(xname "testsuite", "")) :> obj
     let doc = XDocument( XDeclaration("1.0", "UTF-8", "yes"), [| elms |])
+    doc.ToString ()
+
+  let createJUnitReport report =
+    let attribute = XAttribute(xname "name", "group")
+    let attribute2 = XAttribute(xname "tests", "group")
+    let suite = XElement(xname "testsuite", "", attribute, attribute2)
+    let suites = XElement(xname "testsuites", suite) :> obj
+    let doc = XDocument( XDeclaration("1.0", "UTF-8", "yes"), [| suites |])
     doc.ToString ()
--- selftests/JUnitFormatterSpecs.fs
+++ selftests/JUnitFormatterSpecs.fs
@@ -1,9 +1,29 @@
 ﻿module JUnitFormatterSpecs
+open System.Xml.Linq
 open XmlHelpers
 open FSpec
 open FSpec.Dsl
 open FSpec.Matchers
 open FSpec.Formatters
+
+let desc name = { Name = name; MetaData = TestDataMap.Zero }
+
+let withOneTestSuite : Matcher<XElement> =
+  let f (actual : XElement) =
+    match actual.Elements() |> List.ofSeq with
+    | [e] -> 
+      match e.Name.LocalName with
+      | "testsuite" -> MatchSuccess e
+      | _ -> MatchFail actual
+    | _ -> MatchFail actual
+  createMatcher f "with one test suite"
+
+let withAttribute name : Matcher<XElement> =
+  let f (actual : XElement) =
+    match actual.Attribute(xname name) with
+    | null -> MatchFail (sprintf "Attribute '%s' was not found" name)
+    | x -> MatchSuccess x.Value
+  createMatcher f (sprintf "with attribute '%s'" name)
 
 let specs =
   describe "JUnitFormatter" [
@@ -11,4 +31,20 @@ let specs =
       let result = JUnitFormatter.run ()
       result.Should beValidJUnitXml
     )
+
+    ("input", ExampleGroupReport (desc "group", [ExampleReport (desc "example", Success)]) )**>
+    context "Test contains one group with one test" [
+      subject (fun ctx ->
+        JUnitFormatter.createJUnitReport ctx?input
+      )
+
+      it "creates a 'testsuite' element with one 'testcase' element" (fun ctx ->
+        let report = ExampleGroupReport (desc "group", [ExampleReport (desc "example", Success)])
+        let junitReport = JUnitFormatter.createJUnitReport report
+        let withNameAttribute = withAttribute "name"
+        ctx.Subject.Should (beXml |>> withRootElement "testsuites" |>> withOneTestSuite |>> withNameAttribute |>> equal "group")
+      )
+
+      itShould beValidJUnitXml
+    ]
   ]
