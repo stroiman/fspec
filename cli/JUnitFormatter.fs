10e8c8c3365bef821b2aad63ddf1dc5e71af1766
Peter Strøiman <peter@stroiman.com>
2014-12-17 09:51:07 +0100

    Use XDocument for xml generation

--- cli/JUnitFormatter.fs
+++ cli/JUnitFormatter.fs
@@ -1,4 +1,11 @@
 ﻿module FSpec.Formatters
+open System.Xml.Linq
+
+let xname = XName.Get
 
 module JUnitFormatter =
-  let run () = """<?xml version="1.0" encoding="UTF-8"?><testsuites><testsuite name="n" tests="0" /></testsuites>"""
+  let run () = 
+    let suite = XElement(xname "testsuite", "")
+    let elms = XElement(xname "testsuites", XElement(xname "testsuite", "")) :> obj
+    let doc = XDocument( XDeclaration("1.0", "UTF-8", "yes"), [| elms |])
+    doc.ToString ()
