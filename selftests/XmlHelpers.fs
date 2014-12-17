ded3b386e306bee22c72991d0de9ea28331fa841
Peter Strøiman <peter@stroiman.com>
2014-12-17 16:07:41 +0100

    Matcher for verifying root element name

--- selftests/XmlHelpers.fs
+++ selftests/XmlHelpers.fs
@@ -1,12 +1,14 @@
 ﻿module XmlHelpers
 open FSpec.Matchers
 open System.Xml
+open System.Xml.Linq
 open System.Xml.Schema
 open System.Reflection
 
 let assembly = Assembly.GetExecutingAssembly()
 let resourceName = "JUnit.xsd"
 let openSchemaStream () = assembly.GetManifestResourceStream(resourceName)
+let xname = XName.Get
 
 let validateJUnitXml xml =
   let messages = ref []
@@ -33,3 +35,12 @@ let beValidJUnitXml =
     | [] -> MatchSuccess ""
     | _ -> MatchFail issues
   createMatcher f "be valid JUnit xml"
+
+let withRootElement name =
+  let f (actual : XDocument) =
+    let root = actual.Root
+    if root.Name = xname name then
+      MatchSuccess actual.Root
+    else
+      MatchFail root
+  createMatcher f (sprintf "with root element: '%s'" name)
