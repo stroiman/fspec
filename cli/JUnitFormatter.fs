3a31a218165b46ed958d633e60a60634f51593fb
Peter Strøiman <peter@stroiman.com>
2015-01-06 15:04:10 +0100

    Write to a provided stream
    This allows the JUnitFormatter ignore where the data is written. During
    normal run, data can be written to a file, and during testing, data can be
    written to memory

--- cli/JUnitFormatter.fs
+++ cli/JUnitFormatter.fs
@@ -3,11 +3,17 @@ open System.Xml.Linq
 
 let xname = XName.Get
 
-module JUnitFormatter =
-  let run () = 
+type JUnitFormatter (stream:System.IO.Stream) =
+  let write (doc:XDocument) =
+    let settings = System.Xml.XmlWriterSettings()
+    settings.Encoding <- System.Text.UTF8Encoding(false)
+    use writer = System.Xml.XmlWriter.Create(stream, settings)
+    doc.WriteTo(writer)
+
+  member __.Run () = 
     let name = XAttribute(xname "name", "name")
     let tests = XAttribute(xname "tests", "0")
     let suite = XElement(xname "testsuite", name, tests)
     let elms = XElement(xname "testsuites", suite) :> obj
     let doc = XDocument( XDeclaration("1.0", "UTF-8", "yes"), [| elms |])
-    doc.ToString ()
+    write doc
--- selftests/JUnitFormatterSpecs.fs
+++ selftests/JUnitFormatterSpecs.fs
@@ -5,10 +5,16 @@ open FSpec.Dsl
 open FSpec.Matchers
 open FSpec.Formatters
 
+let run () =
+    use stream = new System.IO.MemoryStream()
+    let formatter = JUnitFormatter(stream)
+    formatter.Run()
+    System.Text.Encoding.UTF8.GetString (stream.ToArray())
+
 let specs =
   describe "JUnitFormatter" [
     it "creates valid xml" (fun _ ->
-      let result = JUnitFormatter.run ()
+      let result = run ()
       result.Should beValidJUnitXml
     )
   ]
