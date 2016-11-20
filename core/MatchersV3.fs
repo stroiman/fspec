749a31219513e14cee01b0e5e056f32babe8f4ca
Peter Strøiman <peter@stroiman.com>
2016-11-20 19:34:56 +0100

    Remove single case discriminated union
    This just made it more complex to create a matcher

--- core/MatchersV3.fs
+++ core/MatchersV3.fs
@@ -12,31 +12,25 @@ type MatchResult<'TSuccess,'TFail> =
 
 // Generic matcher type, takes an actual value as input
 // and returns a match result as output
-type M<'a,'b,'c> = M of ('a -> MatchResult<'b,'c>)
+type Matcher<'a,'b,'c> = 'a -> MatchResult<'b,'c>
 
-module M =
-    let run (M matcher) actual = actual |> matcher
+module Matcher =
     let bind f x =
         match x with
         | Success(x,_) -> f x
         | Failure(x,y) -> Failure(x,y)
 
-let (>=>) (M f) (M g) = f >> M.bind g |> M
+let (>=>) f g = f >> Matcher.bind g
 
-let equal expected =
-    let m actual =
-        if actual = expected then
-            Success(actual, sprintf "equal %A" expected)
-        else
-            Failure(actual, sprintf "equal %A" expected)
-    M m
+let equal expected actual =
+    if actual = expected then
+        Success(actual, sprintf "equal %A" expected)
+    else
+        Failure(actual, sprintf "equal %A" expected)
 
-let haveLength =
-    let m actual =
-        Success(actual |> Seq.length, "have length")
-    M m
+let haveLength actual = Success(actual |> Seq.length, "have length")
 
 let should matcher actual = 
-    actual |> M.run matcher |> function
+    actual |> matcher |> function
     | Success _ -> ()
     | Failure _ -> raise (AssertionError { Message = "failed" })
